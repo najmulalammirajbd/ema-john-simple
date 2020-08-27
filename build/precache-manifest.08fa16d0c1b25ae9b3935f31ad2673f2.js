0f2391c71f8e75919fdc34ca70a475376fc3d3be
najmulalammirajbd <najmulalammiraj@gmail.com>
2020-08-27 07:50:35 +0600

    my work done

---
+++ build/precache-manifest.08fa16d0c1b25ae9b3935f31ad2673f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a22c4c3a79ca4cdae7fd03afc75621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8cbb0b736e4b427eb67",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4b76f7b075932a54290b",
+    "url": "/static/css/main.303bd0bd.chunk.css"
+  },
+  {
+    "revision": "d8cbb0b736e4b427eb67",
+    "url": "/static/js/2.cae836cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cae836cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b76f7b075932a54290b",
+    "url": "/static/js/main.c4b8dba7.chunk.js"
+  },
+  {
+    "revision": "60d21ee8588f46c03654",
+    "url": "/static/js/runtime-main.e64453f0.js"
+  },
+  {
+    "revision": "cbe45bcfce30b4f9cbd620026b478f89",
+    "url": "/static/media/logo.cbe45bcf.png"
+  }
+]);
